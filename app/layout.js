2aece9f39bfec37795c35d6febf6c439040fcaad
Chahraoui Idriss <chahraouiidriss@gmail.com>
2025-06-11 21:23:17 +0200

    Ready for production deployment

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import './globals.css';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function RootLayout({ children }) {
   return (
@@ -19,6 +20,7 @@ export default function RootLayout({ children }) {
           {children}
           <Footer />
           <Analytics />
+          <SpeedInsights />
         </Providers>
       </body>
     </html>
