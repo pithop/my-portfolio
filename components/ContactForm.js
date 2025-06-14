8eabcab147203ec1db7cb4087f9522aaf767d991
Chahraoui Idriss <chahraouiidriss@gmail.com>
2025-06-15 01:17:01 +0200

    test

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -23,7 +23,7 @@ export default function ContactForm() {
     setIsSubmitting(true);
     
     try {
-      const response = await fetch('https://formspree.io/f/mvoegwbg', {
+      const response = await fetch('https://formspree.io/f/xrbkkpkr', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
