cdfc5101852bb3c6d1294330dd4fef2c3ff823af
Chahraoui Idriss <chahraouiidriss@gmail.com>
2025-06-19 21:45:50 +0200

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,18 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
+  darkMode: 'class', // Make sure dark mode is enabled
   theme: {
-    extend: {},
+    extend: {
+      // Define your custom colors here
+      colors: {
+        dark: '#0f172a',    // Your custom dark color
+        light: '#f8fafc',   // Your custom light color
+        primary: '#6366f1', // Your custom primary color
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/typography')
   ],
-  darkMode: 'class',
 }
